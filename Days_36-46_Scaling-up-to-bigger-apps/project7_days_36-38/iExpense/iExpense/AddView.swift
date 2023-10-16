b0e80181f0b1173637daad457f5fe0ae84a81d18
jpittz <jamespittman1000@gmail.com>
2023-10-16 12:16:14 +0100

    day37

---
+++ Days_36-46_Scaling-up-to-bigger-apps/project7_days_36-38/iExpense/iExpense/AddView.swift
@@ -0,0 +1,48 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by James on 16/10/2023.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @ObservedObject var expenses: Expenses
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    
+    let types = ["Business", "Personal"]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount", value: $amount, format: .currency(code: "GBP"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(item)
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    AddView(expenses: Expenses())
+}
--- Days_36-46_Scaling-up-to-bigger-apps/project7_days_36-38/iExpense/iExpense/ContentView.swift
+++ Days_36-46_Scaling-up-to-bigger-apps/project7_days_36-38/iExpense/iExpense/ContentView.swift
@@ -7,113 +7,42 @@
 
 import SwiftUI
 
-class User: ObservableObject { // must be type observable
-    @Published var firstName = "Bilbo"
-    @Published var lastName = "Baggins"
-} // published to be seen by StateObject
-
-struct UserContentView: View {
-    @StateObject var user = User()
-        // allows published state changes in object to be seen
-    
-    var body: some View {
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName)")
-            // $ is a two way binding
-            TextField("First name", text: $user.firstName)
-            TextField("Last name", text: $user.lastName)
-        }
-        .padding()
-    }
-}
-
-struct SecondView: View {
-    @Environment(\.dismiss) var dismiss
-    
-    let name: String
-    
-    var body: some View {
-        VStack {
-            Text("Hello \(name)!")
-            Button("Dismiss") {
-                dismiss()
-            }
-        }
-    }
-}
-
-struct FirstView: View {
-    @State private var showingSheet = false
-    
-    var body: some View {
-        Button("Show sheet") {
-            showingSheet.toggle()
-        }
-        .sheet(isPresented: $showingSheet) {
-            SecondView(name: "James")
-        }
-    }
-}
-
-struct DeleteContent: View {
-    @State private var numbers = [Int]()
-    @State private var currentNumber = 1
+struct ContentView: View {
+    @StateObject var expenses = Expenses()
+    @State private var showingAddExpense = false
     
     var body: some View {
         NavigationView {
-            VStack {
-                List {
-                    ForEach(numbers, id: \.self) {
-                        Text("Row \($0)")
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        
+                        Spacer()
+                        Text(item.amount, format: .currency(code: "GBP"))
                     }
-                    .onDelete(perform: removeRows)
                 }
-                
-                Button("Add Number") {
-                    numbers.append(currentNumber)
-                    currentNumber += 1
-                }
+                .onDelete(perform: removeItems)
             }
-            .navigationTitle("onDelete()")
+            .navigationTitle("iExpense")
             .toolbar {
-                EditButton()
+                Button {
+                    showingAddExpense = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
             }
         }
     }
-    
-    func removeRows(at offsets: IndexSet) {
-        numbers.remove(atOffsets: offsets)
-    }
-}
-
-struct SavingDataView: View {
-    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
-    
-    var body: some View {
-        Button("Tap count: \(tapCount)") {
-            tapCount += 1
-            UserDefaults.standard.set(tapCount, forKey: "Tap")
-        }
-    }
-}
-
-struct User2: Codable {
-    let firstName: String
-    let lastName: String
-}
-
-struct ContentView: View {
-    @State private var user = User2(firstName: "James", lastName: "Boblo")
-    
-    var body: some View {
-        Button("Save user") {
-            let encoder = JSONEncoder()
-            
-            // data is datatype called 'Data'
-            if let data = try? encoder.encode(user) {
-                UserDefaults.standard.set(data, forKey: "UserData")
-            }
-        }
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
