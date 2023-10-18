90a8e98bcb91513e246400c1427dff7e84667d91
jpittz <jamespittman1000@gmail.com>
2023-10-18 19:14:57 +0100

    day35 progress, still unfinished

--- Days_26-35_Expanding-your-skills/challenge_day_35/Edutainment/Edutainment/ContentView.swift
+++ Days_26-35_Expanding-your-skills/challenge_day_35/Edutainment/Edutainment/ContentView.swift
@@ -7,37 +7,81 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var gameRunning = false
-    @State private var questions = [Int]()
-    @State private var chosenTimesTable = 2
-    @State private var noOfQuestions = 5
-    @State private var currentQuestion = 1
+struct GameView: View {
+    @State private var question = 1
+    @State private var userAnswer = 0
     
+    @Binding var chosenTimesTable: Int
+    @Binding var noOfQuestions: Int
+    
+    var questionTimesTable: [Int] = randomQuestionGen()  // TO FIX
     
     var body: some View {
-        VStack {
-            HStack {
-                Image(systemName: "multiply.circle")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 30, height: 30)
-                    .padding()
-                    
-                Stepper("\(chosenTimesTable) Times Table", value: $chosenTimesTable, in: 2...12)
-                    .fixedSize()
+        NavigationView {
+            VStack {
+                Text("Question \(question)")
+                Text("What is \(questionTimesTable[question]) x \(chosenTimesTable) ?")
+                
+                TextField("Answer", value: $userAnswer, format: .number)
+                
+                if userAnswer == (questionTimesTable[question] * chosenTimesTable) {
+                    Text("Correct")
+                } else {
+                    Text("Incorrect")
+                }
             }
-            Button("Play game!") {
-                startGame()
-            }
-            .contentShape(RoundedRectangle(cornerRadius: 70))
-            .foregroundColor(.blue)
         }
-        .padding()
     }
-    func startGame() {
-        for _ in 1..<noOfQuestions {
-            questions.append(Int.random(in: 1..<12))
+    
+    func randomQuestionGen() -> [Int] {
+        var randomTimesTable = [Int]()
+        
+        for _ in 1...12 {
+            randomTimesTable.append(Int.random(in: 1...12))
+        }
+        
+        return randomTimesTable
+    }
+}
+
+struct ContentView: View {
+    @State private var chosenTimesTable = 2
+    @State private var noOfQuestions = 10
+    @State private var currentQuestion = 1
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                HStack {
+                    Image(systemName: "multiply.circle")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 30, height: 30)
+                        .padding()
+                    
+                    Stepper("\(chosenTimesTable) Times Table", value: $chosenTimesTable, in: 2...12)
+                        .fixedSize()
+                }
+                HStack {
+                    Image(systemName: "questionmark.circle")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 30, height: 30)
+                        .padding()
+                    
+                    Stepper("\(noOfQuestions) Questions", value: $noOfQuestions, in: 2...12)
+                        .fixedSize()
+                }
+                NavigationLink {
+                    GameView()
+                } label: {
+                    Text("Play Game!")
+                }
+                .contentShape(RoundedRectangle(cornerRadius: 70))
+                .foregroundColor(.blue)
+            }
+            .navigationTitle("Edutainment")
+            .padding()
         }
     }
 }
