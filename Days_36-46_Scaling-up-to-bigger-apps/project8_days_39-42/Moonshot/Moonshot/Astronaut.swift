551a5caa4c5f50b03a807c2a5ad1355232cc9e0d
jpittz <jamespittman1000@gmail.com>
2023-10-19 17:02:37 +0100

    day40

---
+++ Days_36-46_Scaling-up-to-bigger-apps/project8_days_39-42/Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by James on 19/10/2023.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
